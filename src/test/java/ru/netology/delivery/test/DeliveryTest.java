52f159311e585e218a578c2315474ad8d0a76136
nrr <nrr@warsong.me>
2022-08-04 04:26:08 +0700

    change code after previous homework feedback

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -35,9 +35,9 @@ public class DeliveryTest {
         $x("//input[@name='name']").setValue(validUser.getName());
         $x("//input[@name='phone']").setValue(validUser.getPhone());
         $x("//*[@data-test-id='agreement']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(0).shouldBe(visible);
-        $$("[class='input__sub']").get(0).shouldHave(exactText("Поле обязательно для заполнения"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='city'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -50,8 +50,8 @@ public class DeliveryTest {
         $x("//input[@name='phone']").setValue(validUser.getPhone());
         $x("//*[@data-test-id='agreement']").click();
         $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(1).shouldBe(visible);
-        $$("[class='input__sub']").get(1).shouldHave(exactText("Неверно введена дата"));
+        $("[data-test-id='date'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Неверно введена дата"));
     }
 
     @Test
@@ -64,9 +64,9 @@ public class DeliveryTest {
         $x("//*[@placeholder='Дата встречи']").setValue(meetingDate);
         $x("//input[@name='phone']").setValue(validUser.getPhone());
         $x("//*[@data-test-id='agreement']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(2).shouldBe(visible);
-        $$("[class='input__sub']").get(2).shouldHave(exactText("Поле обязательно для заполнения"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='name'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -80,8 +80,9 @@ public class DeliveryTest {
         $x("//input[@name='name']").setValue(validUser.getName());
         $x("//*[@data-test-id='agreement']").click();
         $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(3).shouldBe(visible);
-        $$("[class='input__sub']").get(3).shouldHave(exactText("Поле обязательно для заполнения"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='phone'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -94,10 +95,9 @@ public class DeliveryTest {
         $x("//*[@placeholder='Дата встречи']").setValue(meetingDate);
         $x("//input[@name='name']").setValue(validUser.getName());
         $x("//input[@name='phone']").setValue(validUser.getPhone());
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $("[data-test-id='agreement'].input_invalid").shouldBe(visible);
-        $("[data-test-id='agreement'].input_invalid").shouldHave(
-                exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='agreement'].input_invalid").shouldBe(visible)
+                .shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
     }
 
     @Test
@@ -111,9 +111,9 @@ public class DeliveryTest {
         $x("//input[@name='name']").setValue(validUser.getName());
         $x("//input[@name='phone']").setValue(validUser.getPhone());
         $x("//*[@data-test-id='agreement']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(0).shouldBe(visible);
-        $$("[class='input__sub']").get(0).shouldHave(exactText("Доставка в выбранный город недоступна"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='city'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Доставка в выбранный город недоступна"));
     }
 
     @Test
@@ -126,9 +126,9 @@ public class DeliveryTest {
         $x("//input[@name='name']").setValue(validUser.getName());
         $x("//input[@name='phone']").setValue(validUser.getPhone());
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(1).shouldBe(visible);
-        $$("[class='input__sub']").get(1).shouldHave(exactText("Неверно введена дата"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='date'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Неверно введена дата"));
     }
 
     @Test
@@ -142,9 +142,8 @@ public class DeliveryTest {
         $x("//input[@name='name']").setValue(validUser.getName());
         $x("//input[@name='phone']").setValue(validUser.getPhone());
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(2).shouldBe(visible);
-        $$("[class='input__sub']").get(2).shouldHave(
+        $(".button [class='button__text']").click();
+        $("[data-test-id='name'] .input__sub").shouldBe(visible).shouldHave(
                 exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
@@ -164,7 +163,7 @@ public class DeliveryTest {
         $x("//input[@name='name']").setValue(validUser.getName());
         $x("//input[@name='phone']").setValue(validUser.getPhone());
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $(".button [class='button__text']").click();
         $x("//*[@data-test-id='success-notification']").shouldBe(visible, Duration.ofSeconds(5));
         $x("//*[@data-test-id='success-notification']").should(Condition.matchText("Успешно!"));
         $("[data-test-id=success-notification] .notification__content").shouldBe(visible, Duration.ofSeconds(5))
@@ -185,12 +184,12 @@ public class DeliveryTest {
         $x("//input[@name='name']").setValue(validUser.getName());
         $x("//input[@name='phone']").setValue(validUser.getPhone());
         $x("//*[@data-test-id='agreement']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $(".button [class='button__text']").click();
         $x("/html/body/div[1]/div/div[1]/div[3]").shouldBe(visible, Duration.ofSeconds(5))
                 .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate));
         $x("//*[@placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
         $x("//*[@placeholder='Дата встречи']").setValue(secondMeetingDate);
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $(".button [class='button__text']").click();
         $(withText("Перепланировать")).click();
         $("[data-test-id=success-notification] .notification__content").shouldBe(visible, Duration.ofSeconds(5))
                 .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
